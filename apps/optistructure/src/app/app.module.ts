3814565d89cd5790af157684205db8fad165a49a
Christodoulos Fragkoudakis <chfrag@mail.ntua.gr>
2021-01-28 00:06:42 +0200

    nx add @datorama/akita

--- apps/optistructure/src/app/app.module.ts
+++ apps/optistructure/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, environment.production ? [] : AkitaNgDevtools.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
