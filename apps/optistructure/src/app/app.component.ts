8be5c5e3e046a3d3df73be7867c0d0c59fbef634
Christodoulos Fragkoudakis <chfrag@mail.ntua.gr>
2021-01-27 20:29:35 +0200

    initial commit

---
+++ apps/optistructure/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'infinite-loops-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'optistructure';
+}
