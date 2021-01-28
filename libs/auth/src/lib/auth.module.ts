a6f81901f099276f41deb092fd00241b6b35dc9c
Christodoulos Fragkoudakis <chfrag@mail.ntua.gr>
2021-01-28 13:12:58 +0200

    sign-in component primer

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { UiModule } from '@infinite-loops/ui';
+
+import { SignInComponent } from './sign-in/sign-in.component';
+
 @NgModule({
   imports: [
     CommonModule,
+    UiModule,
 
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      { path: 'sign-in', component: SignInComponent },
     ]),
   ],
+  declarations: [SignInComponent],
 })
 export class AuthModule {}
