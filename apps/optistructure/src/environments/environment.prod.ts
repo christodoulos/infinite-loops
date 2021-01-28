1e2edc6e500ed8a00290605c5bdee97513257f1f
Christodoulos Fragkoudakis <chfrag@mail.ntua.gr>
2021-01-28 11:27:50 +0200

    firebase.sdk

--- apps/optistructure/src/environments/environment.prod.ts
+++ apps/optistructure/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
+import { firebase } from './firebase.sdk';
 export const environment = {
   production: true,
+  firebase,
 };
---
+++ apps/optistructure/src/environments/firebase.sdk.ts
@@ -0,0 +1,9 @@
+export const firebase = {
+  apiKey: 'AIzaSyCovhsuQzqnSsc7FGooxESPbFW9AmCrnGI',
+  authDomain: 'optistructure-cloud.firebaseapp.com',
+  projectId: 'optistructure-cloud',
+  storageBucket: 'optistructure-cloud.appspot.com',
+  messagingSenderId: '431293397002',
+  appId: '1:431293397002:web:57fff403493321bc7607f3',
+  measurementId: 'G-QENL3TJN2B',
+};
