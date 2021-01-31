4f9f1bdd20efedecb37f136312baa3a3d378b255
Christodoulos Fragkoudakis <chfrag@mail.ntua.gr>
2021-01-31 11:50:05 +0200

    deleted nx lib optistructure-common

--- libs/optistructure/optistructure-common/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './lib/optistructure-ui.module';
-export * from './lib/topbar/topbar.component';
-export * from './lib/topbar-user/topbar-user.component';
-export * from './lib/sidebar/sidebar.component';
-export * from './lib/sidebar-user/sidebar-user.component';
-export * from './lib/landing/landing.component';
-export * from './lib/not-found/not-found.component';
