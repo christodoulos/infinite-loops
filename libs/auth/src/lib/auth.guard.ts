87d6ae7f72fca0642bf87abd4524e09f529fea94
Christodoulos Fragkoudakis <chfrag@mail.ntua.gr>
2021-02-07 19:58:16 +0200

    auth-guard cleanup

--- libs/auth/src/lib/auth.guard.ts
+++ libs/auth/src/lib/auth.guard.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router,
-} from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -13,15 +8,8 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authService: AuthService) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean {
-    console.log('AUTH GUARD');
+  canActivate(): boolean {
     if (!this.authService.isLoggedIn) {
-      // this.router.navigate(['sign-in'], {
-      //   queryParams: { returnUrl: state.url },
-      // });
       this.router.navigate(['sign-in']);
     }
     return true;
